76e7fe4a17d1fb6686b598bbe8703d171dd86297
Leo Mancini <mancini@leo.gd>
2022-04-22 14:19:30 -0400

    Updating timeout

--- makeAndPublishPrediction.js
+++ makeAndPublishPrediction.js
@@ -29,7 +29,7 @@ async function publishPrediction() {
 
   await driver.get('http://localhost/sunset-quality-predictor/model/predict.html#today');
 
-  let minutesToKeepPageOpen = 5;
+  let minutesToKeepPageOpen = 4;
 
   await driver.sleep(minutesToKeepPageOpen * 60 * 1000);
 
